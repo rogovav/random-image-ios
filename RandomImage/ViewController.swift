38f4283ec117d277af5b8ebbef416a6aa858e5e1
Rogov Alexander <a.rogov@icode.by>
2022-04-03 08:02:50 +0300

    Initial Commit

---
+++ RandomImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomImage
+//
+//  Created by Alexander Rogov on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
